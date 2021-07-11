54a1a6119666e3da163ddbc25637bd60b5bf4b37
Solotory <Joshua1504.jr@gmail.com>
2021-07-11 10:30:39 -0400

    now the kits will be loaded

--- game/src/main/java/net/astrocube/skywars/loader/RepositoryLoader.java
+++ game/src/main/java/net/astrocube/skywars/loader/RepositoryLoader.java
@@ -5,16 +5,18 @@ import net.astrocube.api.core.loader.Loader;
 import net.astrocube.skywars.api.cage.Cage;
 import net.astrocube.skywars.api.chest.tier.ChestTier;
 import net.astrocube.skywars.api.custom.CustomItemRepository;
+import net.astrocube.skywars.api.kit.Kit;
 
 public class RepositoryLoader implements Loader {
 
 	private @Inject CustomItemRepository<ChestTier> tierRepository;
 	private @Inject CustomItemRepository<Cage> cageRepository;
+	private @Inject CustomItemRepository<Kit> kitRepository;
 
 	@Override
 	public void load() {
 		tierRepository.generate();
 		cageRepository.generate();
+		kitRepository.generate();
 	}
-
 }
