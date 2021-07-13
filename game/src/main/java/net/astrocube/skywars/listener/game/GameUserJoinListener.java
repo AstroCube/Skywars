114c3a07806fb976fc35cd90be0ead5e3d424ad3
Solotory <Joshua1504.jr@gmail.com>
2021-07-13 15:07:28 -0400

    added delay on give kit gadget when a player joins

--- game/src/main/java/net/astrocube/skywars/listener/game/GameUserJoinListener.java
+++ game/src/main/java/net/astrocube/skywars/listener/game/GameUserJoinListener.java
@@ -5,31 +5,39 @@ import me.yushust.message.MessageHandler;
 import net.astrocube.api.bukkit.game.event.game.GameUserJoinEvent;
 import net.astrocube.api.bukkit.game.match.UserMatchJoiner;
 import net.astrocube.api.bukkit.user.inventory.nbt.NBTUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import team.unnamed.gui.core.item.type.ItemBuilder;
 
 public class GameUserJoinListener implements Listener {
 
+	private @Inject Plugin plugin;
 	private @Inject MessageHandler messageHandler;
 
-	@EventHandler(priority = EventPriority.MONITOR)
 	public void onJoin(GameUserJoinEvent event) {
 		Player player = event.getPlayer();
 
 		if (event.getOrigin() == UserMatchJoiner.Origin.WAITING) {
-			ItemStack gadget = ItemBuilder.newBuilder(Material.STONE_SWORD)
-				.setName(messageHandler.get(player, "gadget.kit-select.name"))
-				.setLore(messageHandler.get(player, "gadget.kit-select.lore"))
-				.build();
-			gadget = NBTUtils.addString(gadget, "actionable", "kit_select");
-			player.getInventory().setItem(
-				0,
-				gadget
+			Bukkit.getScheduler().runTaskLater(
+				plugin,
+				() -> {
+					ItemStack gadget = ItemBuilder.newBuilder(Material.STONE_SWORD)
+						.setName(messageHandler.get(player, "gadget.kit-select.name"))
+						.setLore(messageHandler.get(player, "gadget.kit-select.lore"))
+						.build();
+					gadget = NBTUtils.addString(gadget, "actionable", "kit_select");
+					player.getInventory().setItem(
+						0,
+						gadget
+					);
+				},
+				20
 			);
 		}
 	}
